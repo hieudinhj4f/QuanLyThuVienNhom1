5d78403fa907b2d8ce7e3395208f37f1c53cf312
hieudinhj4f <dinhhieunguyen248@gmail.com>
2024-03-06 12:42:48 +0700

    Click here

---
+++ QuanLySach/Forgot-Password .cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySach
+{
+    public partial class Forgot_Password : Form
+    {
+        public Forgot_Password()
+        {
+            InitializeComponent();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ QuanLySach/Login Form.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLySach
+{
+    public partial class Login_Form : Form
+    {
+        SqlConnection conn = new SqlConnection("Data Source=localhost;Initial Catalog=QuanLyThuVien;IntergratedSecurity=True");
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader reader;
+
+        public Login_Form()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Form_Load(object sender, EventArgs e)
+        {
+            conn.Open()
+
+        }
+    }
+}
